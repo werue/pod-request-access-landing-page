a2aa2a6cc588acca2e69d5e2bbbb4e92bca59542
W. E. Rue <rue.wev@protonmail.com>
2024-05-20 01:03:47 -0700

    added error message when incorrect email is submitted

--- code/app.js
+++ code/app.js
@@ -1,9 +1,11 @@
 const form = document.querySelector(".registration");
 const input = document.querySelector(".registration__input");
+const errorMessage = document.querySelector(".emailErrorText");
 const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
 form.addEventListener("submit", (e) => {
     if(!emailRegex.test(input.value)){
+    errorMessage.style.display = "block";
     e.preventDefault();
     }
 
